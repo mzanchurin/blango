c3c381115c6791e5ef1e7c3dfac7ad70ea54ce7d
Maxim Zanchurin <maxim.zanchurin@gmail.com>
2022-08-17 07:16:49 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -196,12 +196,15 @@ class Dev(Configuration):
 
 
     REST_FRAMEWORK = {
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework.authentication.BasicAuthentication",
-            "rest_framework.authentication.SessionAuthentication",
-            "rest_framework.authentication.TokenAuthentication",
-        ]
-    }    
+      "DEFAULT_AUTHENTICATION_CLASSES": [
+          "rest_framework.authentication.BasicAuthentication",
+          "rest_framework.authentication.SessionAuthentication",
+          "rest_framework.authentication.TokenAuthentication",
+      ],
+      "DEFAULT_PERMISSION_CLASSES": [
+          "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+      ],
+    }  
 
 class Prod(Dev):
     DEBUG = True
