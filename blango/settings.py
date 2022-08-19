5fabf2b8d2db765ffbfc795b9e2292a637a60e0f
Maxim Zanchurin <maxim.zanchurin@gmail.com>
2022-08-19 15:15:47 +0000

    Finish throttling

--- blango/settings.py
+++ blango/settings.py
@@ -197,14 +197,26 @@ class Dev(Configuration):
 
 
     REST_FRAMEWORK = {
-      "DEFAULT_AUTHENTICATION_CLASSES": [
-          "rest_framework.authentication.BasicAuthentication",
-          "rest_framework.authentication.SessionAuthentication",
-          "rest_framework.authentication.TokenAuthentication",
-      ],
-      "DEFAULT_PERMISSION_CLASSES": [
-          "rest_framework.permissions.IsAuthenticatedOrReadOnly"
-      ],
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
+        "DEFAULT_THROTTLE_CLASSES": [
+            "blog.api.throttling.AnonSustainedThrottle",
+            "blog.api.throttling.AnonBurstThrottle",
+            "blog.api.throttling.UserSustainedThrottle",
+            "blog.api.throttling.UserBurstThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "anon_sustained": "500/day",
+            "anon_burst": "10/minute",
+            "user_sustained": "5000/day",
+            "user_burst": "100/minute",
+        },
     }  
 
 
@@ -214,6 +226,10 @@ class Dev(Configuration):
             "Basic": {"type": "basic"},
         }
     }    
+
+
+
+
 
 class Prod(Dev):
     DEBUG = True
