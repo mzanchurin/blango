fe4f75c1d73695df8dcfa33e4daaf2b7a4f328b6
Maxim Zanchurin <maxim.zanchurin@gmail.com>
2022-08-22 12:58:41 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
     path("ip/", blog.views.get_ip),
     path("accounts/", include("allauth.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
